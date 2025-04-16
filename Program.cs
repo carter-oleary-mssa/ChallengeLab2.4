b01c47d5be8acc6d320a6ff88d800dfa8256cf45
carter-oleary <carter.m.oleary@outlook.com>
2025-04-16 17:39:42 -0400

    Updated code to implement class

--- Program.cs
+++ Program.cs
@@ -7,19 +7,20 @@ name = Console.ReadLine();
 Console.Write("Input the marks of Physics, Chemistry, and Computer Application: ");
 string[] temp = Console.ReadLine().Split(' ');
 int[] marks = new int[temp.Length];
-int sum = 0;
-for(int i  = 0; i < temp.Length; i++)
+for (int i = 0; i < temp.Length; i++)
 {
     marks[i] = int.Parse(temp[i]);
-    sum += marks[i];
 }
 
-Console.WriteLine($"Roll No: {rollNumber}");
-Console.WriteLine($"Name of Student: {name}");
-Console.WriteLine($"Marks in Physics: {marks[0]}");
-Console.WriteLine($"Marks in Chemistry: {marks[1]}");
-Console.WriteLine($"Marks in Computer Application: {marks[2]}");
-Console.WriteLine($"Total Marks = {sum}");
-Console.WriteLine($"Percentage = {Convert.ToDecimal(sum/marks.Length).ToString("f")}");
-Console.WriteLine("Division = First");
+Student s = new(rollNumber, name, marks);
+
+
+Console.WriteLine($"Roll No: {s.StudentId}");
+Console.WriteLine($"Name of Student: {s.StudentName}");
+Console.WriteLine($"Marks in Physics: {s.Marks[0]}");
+Console.WriteLine($"Marks in Chemistry: {s.Marks[1]}");
+Console.WriteLine($"Marks in Computer Application: {s.Marks[2]}");
+Console.WriteLine($"Total Marks = {s.TotalMarks()}");
+Console.WriteLine($"Percentage = {s.GetPercentage().ToString("f")}");
+Console.WriteLine($"Division = {s.GetDivision()}");
 
---
+++ Student.cs
@@ -0,0 +1,49 @@
+﻿class Student
+{
+    public string StudentId { get; set; }
+    public string StudentName { get; set; }
+    public int[] Marks { get; set; }
+
+    public Student(string studentId, string studentName, int[] marks)
+    {
+        StudentId = studentId;
+        StudentName = studentName;
+        Marks = marks;
+    }
+
+    public int TotalMarks()
+    {
+        int sum = 0;
+        foreach(int i in Marks)
+        {
+            sum += i;
+        }
+        return sum;
+    }
+
+    public decimal GetPercentage()
+    {
+        return Convert.ToDecimal(TotalMarks() / Marks.Length);
+    }
+
+    public string GetDivision()
+    {
+        switch (GetPercentage())
+        {
+            case decimal d when (d >= 70):
+                return "First";
+                break;
+            case decimal d when (d >= 60):
+                return "Second, Upper";
+                break;
+            case decimal d when (d >= 50):
+                return "Second, Lower";
+                break;
+            default:
+                return "Third";
+                break;
+
+        }
+    }
+}
+
